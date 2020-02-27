43838c425de09c089aeaea988ee50e43e6bf7a62
Songsheng Tao <st3107@columbia.edu>
2020-02-27 16:22:26 -0500

    TST: change the test cases to the ruamel style

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,11 +14,11 @@ from bibconvert.main import main
     ]
 )
 def test_main(test_case, expect):
-    main(*test_case)
     output_file = Path(test_case[1])
+    main(*test_case)
     expect_file = Path(expect)
-    with output_file.open("r") as f0:
+    with output_file.open("r", encoding="utf-8") as f0:
         output_str = f0.read()
-    with expect_file.open("r") as f1:
+    with expect_file.open("r", encoding="utf-8") as f1:
         expect_str = f1.read()
     assert output_str == expect_str
